f49446f4358c2404cc40a8a8be1c074c449c7f20
Braden Brooks <brooks@Bradens-MacBook-Air.local>
2020-01-06 13:11:36 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,22 @@
+console.log("Up and running!");
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
