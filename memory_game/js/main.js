e0285754623c9b1c8cd1acbd4b97658b908ca0dc
Braden Brooks <brooks@Bradens-MacBook-Air.local>
2020-01-06 22:15:05 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,6 +26,7 @@ const cards = [
 const cardsInPlay = [];
 
 function checkForMatch() {
+	this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert ("You found a match!");
@@ -33,12 +34,26 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert ("Sorry, try again.");
 }}};
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	checkForMatch();
 };
-flipCard(0); 
-flipCard(2);
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+	let cardElement = document.createElement('img');
+	cardElement.setAttribute('src', "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard());
+	cardElement.appendChild('#game-board');
+	}
+};
+createBoard();
+
+
+
+
